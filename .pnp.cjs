d822b8a8ca3bf549aaee0d9843d3fecd94a57d78
TatiORodrigues <tatiana.o.rodrigues@gmail.com>
2025-07-09 16:27:51 +0100

    alterações de correção após prettier write

--- .pnp.cjs
+++ .pnp.cjs
@@ -16280,7 +16280,7 @@ var libzipSync = { exports: {} };
           HEAP32[(buf + 24) >> 2] = stat.gid;
           HEAP32[(buf + 28) >> 2] = stat.rdev;
           HEAP32[(buf + 32) >> 2] = 0;
-          (tempI64 = [
+          ((tempI64 = [
             stat.size >>> 0,
             ((tempDouble = stat.size),
             +Math.abs(tempDouble) >= 1
@@ -16294,7 +16294,7 @@ var libzipSync = { exports: {} };
               : 0),
           ]),
             (HEAP32[(buf + 40) >> 2] = tempI64[0]),
-            (HEAP32[(buf + 44) >> 2] = tempI64[1]);
+            (HEAP32[(buf + 44) >> 2] = tempI64[1]));
           HEAP32[(buf + 48) >> 2] = 4096;
           HEAP32[(buf + 52) >> 2] = stat.blocks;
           HEAP32[(buf + 56) >> 2] = (stat.atime.getTime() / 1e3) | 0;
@@ -16303,7 +16303,7 @@ var libzipSync = { exports: {} };
           HEAP32[(buf + 68) >> 2] = 0;
           HEAP32[(buf + 72) >> 2] = (stat.ctime.getTime() / 1e3) | 0;
           HEAP32[(buf + 76) >> 2] = 0;
-          (tempI64 = [
+          ((tempI64 = [
             stat.ino >>> 0,
             ((tempDouble = stat.ino),
             +Math.abs(tempDouble) >= 1
@@ -16317,7 +16317,7 @@ var libzipSync = { exports: {} };
               : 0),
           ]),
             (HEAP32[(buf + 80) >> 2] = tempI64[0]),
-            (HEAP32[(buf + 84) >> 2] = tempI64[1]);
+            (HEAP32[(buf + 84) >> 2] = tempI64[1]));
           return 0;
         },
         doMsync: function (addr, stream, len, flags, offset) {
@@ -16686,7 +16686,7 @@ var libzipSync = { exports: {} };
             return -61;
           }
           FS.llseek(stream, offset, whence);
-          (tempI64 = [
+          ((tempI64 = [
             stream.position >>> 0,
             ((tempDouble = stream.position),
             +Math.abs(tempDouble) >= 1
@@ -16700,7 +16700,7 @@ var libzipSync = { exports: {} };
               : 0),
           ]),
             (HEAP32[newOffset >> 2] = tempI64[0]),
-            (HEAP32[(newOffset + 4) >> 2] = tempI64[1]);
+            (HEAP32[(newOffset + 4) >> 2] = tempI64[1]));
           if (stream.getdents && offset === 0 && whence === 0)
             stream.getdents = null;
           return 0;
